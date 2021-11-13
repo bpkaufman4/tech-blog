6807489f3544260dc6ddaa5c1ddc1f51daab9d8d
Brian Kaufman <bpkaufman4@gmail.com>
2021-11-13 14:52:59 -0600

    created partials for posts and comments

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -22,7 +22,7 @@ async function commentFormHandler(event) {
         if (response.ok) {
             document.location.reload();
         } else {
-            alert(response.statusText);
+            alert(response.statusText); 
         }
     }
 }
