4c2a966a8d5c731442616b7c11aa4758092135a2
Brian Kaufman <bpkaufman4@gmail.com>
2021-11-15 13:55:00 -0600

    removed auth for register form

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -48,7 +48,7 @@ router.get('/:id', (req, res) => {
         });
 });
 
-router.post('/', getAuth, (req, res) => {
+router.post('/', (req, res) => {
     User.create({
         username: req.body.username,
         email: req.body.email,
